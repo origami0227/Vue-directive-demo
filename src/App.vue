c1ade3ca2792097adc521298f9975659054688bf
origami <1071242996@qq.com>
2022-08-25 10:32:37 +0800

    directives

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-y id="app">
+    <HelloWorld msg="Hello Vue in CodeSandbox!" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  directives: {
+    //自定义指令y
+    y: {
+      inserted(el) {
+        el.addEventListener("click", () => {
+          console.log("y");
+        });
+      },
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
